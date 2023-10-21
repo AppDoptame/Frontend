06bcef480f82fd491221a9d3ee60f4ddd7612de6
Sebastian Velez Galeano <sebastian.velez@avenfor.com>
2023-10-21 11:57:11 -0500

    Merged the login and register back with the front

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,29 @@
 
 import { useRef, useState } from "react";
 import { useRouter } from "next/navigation"; // Importa el hook useRouter
-import './styles.css';
+import "./styles.css";
 
 export default function Login() {
   const signUpButtonRef = useRef<HTMLButtonElement | null>(null);
   const signInButtonRef = useRef<HTMLButtonElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [isSignUp, setIsSignUp] = useState(false);
-  const router = useRouter(); // Inicializa el hook useRouter
+  const router = useRouter();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const emailSignInRef = useRef<HTMLInputElement>(null);
+  const passwordSignInRef = useRef<HTMLInputElement>(null);
+
+  const nameSignUpRef = useRef<HTMLInputElement>(null);
+  const emailSignUpRef = useRef<HTMLInputElement>(null);
+  const passwordSignUpRef = useRef<HTMLInputElement>(null);
+  const celularSignUpRef = useRef<HTMLInputElement>(null);
+  const ciudadSignUpRef = useRef<HTMLInputElement>(null);
+  const departamentoSignUpRef = useRef<HTMLInputElement>(null);
+  const fechaNacimientoSignUpRef = useRef<HTMLInputElement>(null);
+
+  const confirmationCodeRef = useRef<HTMLInputElement>(null);
+  const [registrationEmail, setRegistrationEmail] = useState("");
 
   const handleSignUpClick = () => {
     containerRef?.current?.classList.add("right-panel-active");
@@ -21,48 +36,196 @@ export default function Login() {
     setIsSignUp(false);
   };
 
-  const handleSignIn = () => {
-    // Tu lógica de inicio de sesión aquí
+  const handleSignIn = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
 
-    // Después del inicio de sesión exitoso, redirige a la página principal
-    
-    router.push("/filesUpload");
+    const email = emailSignInRef.current?.value || "";
+    const password = passwordSignInRef.current?.value || "";
+
+    const payload = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch(
+        "https://zlwzst2oc8.execute-api.us-east-1.amazonaws.com/sign-in",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log("logged in successfully");
+        router.push("/filesUpload");
+      } else {
+        console.error(data);
+      }
+    } catch (error) {
+      console.error("There was an error logging in:", error);
+    }
   };
 
-  const handleRegistration = () => {
-    // Tu lógica de registro aquí
+  const handleRegistration = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    const name = nameSignUpRef.current?.value || "";
+    const email = emailSignUpRef.current?.value || "";
+    const password = passwordSignUpRef.current?.value || "";
+    const celular = celularSignUpRef.current?.value || "";
+    const ciudad = ciudadSignUpRef.current?.value || "";
+    const departamento = departamentoSignUpRef.current?.value || "";
+    const fechaNacimiento = fechaNacimientoSignUpRef.current?.value || "";
 
-    // Después del registro exitoso, redirige a la página principal
-    router.push("/filesUpload");
+    const payload = {
+      nombre: name,
+      email: email,
+      password: password,
+      celular: celular,
+      ciudad: ciudad,
+      departamento: departamento,
+      fecha_nacimiento: fechaNacimiento,
+    };
+
+    setRegistrationEmail(email);
+    console.log("Registration payload:", payload);
+
+    try {
+      const response = await fetch(
+        "https://zlwzst2oc8.execute-api.us-east-1.amazonaws.com/sign-up",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log(data);
+        setShowConfirmation(true);
+      } else {
+        console.log(data);
+        console.error(data);
+      }
+    } catch (error) {
+      console.error("There was an error registering:", error);
+    }
+  };
+
+  const handleConfirmation = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    const email = registrationEmail;
+    const confirmationCode = confirmationCodeRef.current?.value || "";
+
+    const payload = {
+      email: email,
+      confirmationCode: confirmationCode,
+    };
+
+    console.log("Confirmation payload:", payload);
+
+    try {
+      const response = await fetch(
+        "https://zlwzst2oc8.execute-api.us-east-1.amazonaws.com/confirm-sign-up",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        setShowConfirmation(false);
+        console.log(data);
+        console.log("confirmed");
+      } else {
+        console.error(data);
+      }
+    } catch (error) {
+      console.error("There was an error confirming:", error);
+    }
   };
 
   return (
-    <div className={`container ${isSignUp ? "right-panel-active" : ""}`} ref={containerRef}>
+    <div
+      className={`container ${isSignUp ? "right-panel-active" : ""}`}
+      ref={containerRef}
+    >
       <div className="form-container sign-up-container">
-        <form action="filesUpload">
-          <h1>Crear Cuenta</h1>
-          <div className="social-container">
-            <a href="#" className="social">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#" className="social">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-          <span>o usa tu correo electrónico para registrarte</span>
-          <input type="text" placeholder="Nombre" />
-          <input type="text" placeholder="Correo electrónico" />
-          <input type="password" placeholder="Contraseña" />
-          <button onClick={handleRegistration}>
-            Registrarse
-          </button>
-        </form>
+        {showConfirmation ? (
+          <form action="#">
+            <h1>Confirmar Registro</h1>
+            <input
+              type="text"
+              placeholder="Código de Confirmación"
+              ref={confirmationCodeRef}
+            />
+            <button onClick={(event) => handleConfirmation(event)}>
+              Confirmar
+            </button>
+          </form>
+        ) : (
+          <form>
+            <h1>Crear Cuenta</h1>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>o usa tu correo electrónico para registrarte</span>
+            <input type="text" placeholder="Nombre" ref={nameSignUpRef} />
+            <input
+              type="text"
+              placeholder="Correo electrónico"
+              ref={emailSignUpRef}
+            />
+            <input type="text" placeholder="Celular" ref={celularSignUpRef} />
+            <input type="text" placeholder="Ciudad" ref={ciudadSignUpRef} />
+            <input
+              type="text"
+              placeholder="Departamento"
+              ref={departamentoSignUpRef}
+            />
+            <input
+              type="text"
+              placeholder="Fecha de Nacimiento"
+              ref={fechaNacimientoSignUpRef}
+            />
+
+            <input
+              type="password"
+              placeholder="Contraseña"
+              ref={passwordSignUpRef}
+            />
+
+            <button onClick={(event) => handleRegistration(event)}>
+              Registrarse
+            </button>
+          </form>
+        )}
       </div>
       <div className="form-container sign-in-container">
-        <form action="filesUpload">
+        <form>
           <h1>Iniciar Sesión</h1>
           <div className="social-container">
             <a href="#" className="social">
@@ -76,27 +239,57 @@ export default function Login() {
             </a>
           </div>
           <span>o usa tu cuenta</span>
-          <input type="text" placeholder="Correo electrónico" />
-          <input type="password" placeholder="Contraseña" />
+          <input
+            type="text"
+            placeholder="Correo electrónico"
+            ref={emailSignInRef}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            ref={passwordSignInRef}
+          />
+
           <a href="#">¿Olvidaste tu contraseña?</a>
-          <button onClick={handleSignIn}>
+          <button onClick={(event) => handleSignIn(event)}>
             Iniciar Sesión
           </button>
         </form>
       </div>
       <div className="overlay-container">
         <div className="overlay">
-          <div className={`overlay-panel overlay-left ${isSignUp ? "" : "right-panel-active"}`}>
+          <div
+            className={`overlay-panel overlay-left ${
+              isSignUp ? "" : "right-panel-active"
+            }`}
+          >
             <h2>¡Bienvenido de nuevo!</h2>
-            <p>Para estar conectado con nosotros, inicia sesión con tu información personal</p>
-            <button className="ghost" ref={signInButtonRef} onClick={handleSignInClick}>
+            <p>
+              Para estar conectado con nosotros, inicia sesión con tu
+              información personal
+            </p>
+            <button
+              className="ghost"
+              ref={signInButtonRef}
+              onClick={handleSignInClick}
+            >
               Iniciar Sesión
             </button>
           </div>
-          <div className={`overlay-panel overlay-right ${isSignUp ? "right-panel-active" : ""}`}>
+          <div
+            className={`overlay-panel overlay-right ${
+              isSignUp ? "right-panel-active" : ""
+            }`}
+          >
             <h2>¡Hola, Amigo!</h2>
-            <p>Ingresa tus detalles personales y comienza tu viaje con nosotros</p>
-            <button className="ghost" ref={signUpButtonRef} onClick={handleSignUpClick}>
+            <p>
+              Ingresa tus detalles personales y comienza tu viaje con nosotros
+            </p>
+            <button
+              className="ghost"
+              ref={signUpButtonRef}
+              onClick={handleSignUpClick}
+            >
               Registrarse
             </button>
           </div>
@@ -105,4 +298,3 @@ export default function Login() {
     </div>
   );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,33 +12,35 @@ export default function Home() {
       <section className={styles.textCard}>
         <h1 className={styles.textCardTitle}>¿Quiénes somos?</h1>
         <p className={styles.textCardDescription}>
-          Somos una herramienta creada para organizar, gestionar, controlar sobrantes y excedentes,
-          insumos de rotación e inventario muerto que son generados por obras civiles y su red de
-          abastecimiento. Hacemos parte de Repón SAS, una empresa colombiana innovadora que le
-          gustan las ideas nuevas, el valor agregado e ir más allá. Creada para contribuir,
-          transformar y ayudar a solucionar los diferentes retos que están ligados a la
-          materialización de proyectos de construcción. La cual, desarrollándose, irá consolidando
-          por etapas.
+          Somos una herramienta creada para organizar, gestionar, controlar
+          sobrantes y excedentes, insumos de rotación e inventario muerto que
+          son generados por obras civiles y su red de abastecimiento. Hacemos
+          parte de Repón SAS, una empresa colombiana innovadora que le gustan
+          las ideas nuevas, el valor agregado e ir más allá. Creada para
+          contribuir, transformar y ayudar a solucionar los diferentes retos que
+          están ligados a la materialización de proyectos de construcción. La
+          cual, desarrollándose, irá consolidando por etapas.
         </p>
       </section>
       <Card
-        imgSrc='/repon/working-pc.webp'
+        imgSrc="/repon/working-pc.webp"
         cardTitle={<>Maximiza productividad con REPON SAS.</>}
-        buttonDescription='Leer más ...'
-        imagePosition='left'
+        buttonDescription="Leer más ..."
+        imagePosition="left"
       />
       <Card
-        imgSrc='/repon/man-drilling.webp'
+        imgSrc="/repon/man-drilling.webp"
         cardTitle={<>Eleva tu proyecto con nosotros.</>}
-        buttonDescription='Leer más ...'
-        imagePosition='right'
+        buttonDescription="Leer más ..."
+        imagePosition="right"
       />
       <Card
-        imgSrc='/repon/men-working.webp'
+        imgSrc="/repon/men-working.webp"
         cardTitle={<>Conéctate y optimiza recursos.</>}
-        buttonDescription='Leer más ...'
-        imagePosition='left'
+        buttonDescription="Leer más ..."
+        imagePosition="left"
       />
+
       <Footer />
     </div>
   );
